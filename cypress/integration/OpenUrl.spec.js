4a15a54b2993480f0cfc65e50411ada761f4e154
dwelsh1 <don@vbreview.com>
2022-07-15 02:11:12 -0700

    updates

---
+++ cypress/integration/OpenUrl.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('Update Urls', () => {
+  it('Open Admin page, update qa.config', () => {
+    cy
+      .visit('http://vbreview.com/cypress.html')
+  })
+})
